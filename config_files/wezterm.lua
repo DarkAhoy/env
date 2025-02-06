ec056efae7c25d60c57c00084d04121b67330acd
DarkAhoy <50747316+DarkAhoy@users.noreply.github.com>
2025-02-07 00:13:59 +0200

    Update wezterm.lua

--- config_files/wezterm.lua
+++ config_files/wezterm.lua
@@ -1,21 +1,10 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local act = wezterm.action
+local vim_moves = require 'keys'
 
-return {
-  -- Set a transparent background with opacity
-  window_background_opacity = 0.8,  
-
-  -- Optional: Enable text opacity for additional effects
-  text_background_opacity = 0.8,   
-
-  -- Set font and other optional configurations
-  font = wezterm.font("JetBrains Mono"), 
-  font_size = 12.0,                     
-  color_scheme = "OneHalfDark",         
-
-  hide_tab_bar_if_only_one_tab = true,
-
-  keys = {
+local config = wezterm.config_builder()
+config.leader = { key = 'e', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
     -- paste from the clipboard
     { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
     { key = 'p', mods = 'SUPER', action = act.ActivateCommandPalette },
@@ -24,9 +13,46 @@ return {
     {key="LeftArrow", mods="CTRL", action=wezterm.action{SendString="\x1bb"}},
     -- Make Option-Right equivalent to Alt-f; forward-word
     {key="RightArrow", mods="CTRL", action=wezterm.action{SendString="\x1bf"}},
-    -- paste from the primary selection
-    -- { key = 'v', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
-  },
 
-  window_decorations = "NONE"
+  {
+    mods   = "LEADER",
+    key    = "\"",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods   = "LEADER",
+    key    = "%",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods = 'LEADER',
+    key = 'z',
+    action = wezterm.action.TogglePaneZoomState
+  },
+  {
+    mods = 'LEADER',
+    key = '[',
+    action = wezterm.action.ActivateCopyMode
+  },
 }
+
+-- Set a transparent background with opacity
+config.window_background_opacity = 0.8
+
+-- Optional: Enable text opacity for additional effects
+config.text_background_opacity = 0.8
+
+-- Set font and other optional configurations
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 12.0
+config.color_scheme = "OneHalfDark"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "NONE"
+
+-- Merge vim_moves into config.keys
+for _, keymap in ipairs(vim_moves) do
+  table.insert(config.keys, keymap)
+end
+
+return config
